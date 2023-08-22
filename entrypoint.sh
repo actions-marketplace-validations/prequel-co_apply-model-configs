fc8813bd09ba8520a95d7e80f553a177fbfa6ecd
niger <niger@prequel.co>
2023-08-22 14:05:43 -0400

    log perms

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,13 @@ set -e
 
 echo "Hello $1"
 mkdir -p /usr/local/bin
+ls -ld /usr/local/bin
+if [ -w "/usr/local/bin" ]; then
+    echo "You have write permissions to /usr/local/bin."
+else
+    echo "You do not have write permissions to /usr/local/bin."
+fi
+
 sudo curl -s -L https://storage.googleapis.com/prequel_binaries/install.sh | bash
 ls /usr/local/bin/
 echo "what is in the prequel directory"
