235f3f0bf64423e8018bb46787b51a7d768f7f3a
niger <niger@prequel.co>
2023-08-22 13:55:06 -0400

    fail immediately

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+set -e
+
 echo "Hello $1"
 mkdir -p /usr/local/bin
+ls /usr/local/bin/
 curl -s -L https://storage.googleapis.com/prequel_binaries/install.sh | bash
-ls cat /usr/local/bin/
+ls /usr/local/bin/
+echo "what is in the prequel directory"
+ls /usr/local/bin/prequel
 cat /usr/local/bin/prequel
 prequel -h
