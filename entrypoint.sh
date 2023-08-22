b1abffe61281712cb13d99c13d8468d2e03af3bc
niger <niger@prequel.co>
2023-08-22 12:44:55 -0400

    try bash entry

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,4 @@
 echo "Hello $1"
 curl -s -L https://storage.googleapis.com/prequel_binaries/install.sh | bash
 sudo chmod 777 /usr/local/bin/prequel
-echo "Execute perms set"
-prequel -h
+bash prequel -h
