8fa09e0e1811ee44d4ea6bc9d0594cb61edf48ff
niger <niger@prequel.co>
2023-08-22 15:15:18 -0400

    closing in on it

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,9 @@ set -e
 
 mkdir -p /usr/local/bin
 if [ "$1" == "true" ]; then
-    wget -O /tmp/install.sh https://storage.googleapis.com/prequel_binaries/install.sh
+    wget --quiet -O /tmp/install.sh https://storage.googleapis.com/prequel_binaries/install.sh
 else
-    wget -O /tmp/install.sh https://storage.googleapis.com/prequel_cli_binaries/install.sh
+    wget --quiet -O /tmp/install.sh https://storage.googleapis.com/prequel_cli_binaries/install.sh
 fi
 
 chmod +x /tmp/install.sh
