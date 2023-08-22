6d722d79eaae3f5a3ee27ac4bcb20b325f5ce37b
niger <niger@prequel.co>
2023-08-22 15:05:04 -0400

    all flags

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,15 @@
 
 set -e
 
-echo "Hello $1"
-mkdir -p /usr/local/bin
 
-wget -O /tmp/install.sh https://storage.googleapis.com/prequel_binaries/install.sh
+mkdir -p /usr/local/bin
+if [ "$1" == "true" ]; then
+    wget -O /tmp/install.sh https://storage.googleapis.com/prequel_binaries/install.sh
+else
+    wget -O /tmp/install.sh https://storage.googleapis.com/prequel_cli_binaries/install.sh
+fi
+
 chmod +x /tmp/install.sh
 /tmp/install.sh
 
-prequel -h
+prequel --host="$2" --api_key="$3" --mode="$4" model apply $5
