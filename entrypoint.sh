30e0670d48a7fbbb09533c321f9d15fb2f20be05
niger <niger@prequel.co>
2023-08-22 13:50:00 -0400

    print debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,6 @@
 echo "Hello $1"
 mkdir -p /usr/local/bin
 curl -s -L https://storage.googleapis.com/prequel_binaries/install.sh | bash
+ls cat /usr/local/bin/
+cat /usr/local/bin/prequel
 prequel -h
